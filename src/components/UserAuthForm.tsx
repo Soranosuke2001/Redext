36976d30bc35fdcd0579e2f219f040e318df5568
Sora <sorazora1.4@gmail.com>
2023-07-24 17:20:20 -0700

    added loading state and google button styling

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -1,14 +1,40 @@
-import { FC } from "react";
+"use client";
+
+import { FC, useState } from "react";
 import { Button } from "./ui/Button";
 import { cn } from "@/lib/utils";
+import { signIn } from "next-auth/react";
+import { Icons } from "./Icons";
 
 // Bu adding this, the custom component is now able to accept all the props that can be accepted by a regular div component
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const loginWithGoogle = async () => {
+    setIsLoading(true);
+
+    try {
+      await signIn("google");
+    } catch (error) {
+      // Alert the user there was an error logging in
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className={cn("flex justify-center", className)} {...props}>
-      <Button>Google</Button>
+      <Button
+        onClick={loginWithGoogle}
+        isLoading={isLoading}
+        size="sm"
+        className="w-full"
+      >
+        {isLoading ? null : <Icons.google className="w-4 h-4 mr-2" />}
+        Google
+      </Button>
     </div>
   );
 };
