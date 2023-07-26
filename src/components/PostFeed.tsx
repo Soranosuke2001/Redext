e4f4b0ee4e6c4376880d0a8a994e78116fa95dfa
Sora <sorazora1.4@gmail.com>
2023-07-26 15:44:58 -0700

    created extended type definition for posts

---
+++ src/components/PostFeed.tsx
@@ -0,0 +1,13 @@
+import { ExtendedPost } from "@/types/db";
+import { FC } from "react";
+
+interface PostFeedProps {
+  initialPosts: ExtendedPost[];
+  subredditName?: string;
+}
+
+const PostFeed: FC<PostFeedProps> = ({ initialPosts, subredditName }) => {
+  return <div>PostFeed</div>;
+};
+
+export default PostFeed;
