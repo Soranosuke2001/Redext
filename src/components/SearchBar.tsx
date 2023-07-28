dc99d7261756aa91665e3c0563e292b3bbf1aa67
Sora <sorazora1.4@gmail.com>
2023-07-27 20:02:29 -0700

    implemented the search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,18 +1,29 @@
 "use client";
 
-import { FC, useState } from "react";
-import { Command, CommandInput } from "./ui/Command";
+import { FC, useCallback, useState } from "react";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "./ui/Command";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { Prisma, Subreddit } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import { Users } from "lucide-react";
+import debounce from "lodash.debounce";
 
 interface SearchBarProps {}
 
 const SearchBar: FC<SearchBarProps> = ({}) => {
   const [userInput, setUserInput] = useState<string>("");
+  const router = useRouter();
 
   const {
-    data: queryResult,
+    data: queryResults,
     refetch,
     isFetched,
     isFetching,
@@ -21,14 +32,22 @@ const SearchBar: FC<SearchBarProps> = ({}) => {
       if (!userInput) return [];
 
       const { data } = await axios.get(`/api/search?q=${userInput}`);
-      return data as Subreddit & {
+      return data as (Subreddit & {
         _count: Prisma.SubredditCountOutputType;
-      };
+      })[];
     },
     queryKey: ["search-query"],
     // Makes the fetch request when the user types
     enabled: false,
   });
+
+  const debounceRequest = useCallback(() => {
+    request();
+  }, []);
+
+  const request = debounce(async () => {
+    refetch();
+  }, 500);
 
   return (
     <Command className="relative rounded-lg border max-w-lg z-50 overflow-visible">
@@ -36,10 +55,34 @@ const SearchBar: FC<SearchBarProps> = ({}) => {
         value={userInput}
         onValueChange={(text) => {
           setUserInput(text);
+          debounceRequest();
         }}
         className="outline-none border-none focus:border-none focus:outline-none ring-0"
         placeholder="Seach Communities..."
       />
+
+      {userInput.length > 0 ? (
+        <CommandList className="absolute bg-white top-full inset-x-0 shadow rounded-b-md">
+          {isFetched && <CommandEmpty>No Results Found</CommandEmpty>}
+          {(queryResults?.length ?? 0) > 0 ? (
+            <CommandGroup heading="Communities">
+              {queryResults?.map((subreddit) => (
+                <CommandItem
+                  onSelect={(e) => {
+                    router.push(`/r/${e}`);
+                    router.refresh();
+                  }}
+                  key={subreddit.id}
+                  value={subreddit.name}
+                >
+                  <Users className="mr-2 h-4 w-4" />
+                  <a href={`/r/${subreddit.name}`}>r/{subreddit.name}</a>
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          ) : null}
+        </CommandList>
+      ) : null}
     </Command>
   );
 };
