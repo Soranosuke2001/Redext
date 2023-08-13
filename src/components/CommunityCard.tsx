5d89e9a341748bb127f15d3f6259921810d6fab0
Sora <sorazora1.4@gmail.com>
2023-08-12 23:35:02 -0700

    now renders the data from database

--- src/components/CommunityCard.tsx
+++ src/components/CommunityCard.tsx
@@ -1,37 +1,56 @@
 import { FC } from "react";
 import { Button } from "./ui/Button";
-import { Subscription } from "@prisma/client";
+import { format } from "date-fns";
 
 interface CommunityCardProps {
-  subscriptions: Subscription[]
-
+  subscriptions: {
+    subredditId: string;
+    subredditName: string;
+    subredditCreator: string;
+    subredditCreationDate: string;
+    subredditMemberCount: number;
+  }[];
 }
 
 const CommunityCard: FC<CommunityCardProps> = ({ subscriptions }) => {
   return (
     <>
-      {subscriptions.map((subscription: any) => (
-        <div className="flex justify-between border border-solid border-slate-500 rounded-lg m-3">
-          {/* Left Side */}
-          <div className="flex flex-col m-2">
-            <span className="text-sm text-zinc-500">
-              {`Created By: u/Soranosuke`}
-            </span>
-            <span className="text-4xl font-ibm-plex-mono">r/testing</span>
-            <div className="my-5" />
-            <div className="">Creation Date: August 23, 2023</div>
-          </div>
+      {subscriptions.map((subscription) => {
+        const creationDate = new Date(subscription.subredditCreationDate);
 
-          {/* Right Side */}
-          <div className="flex flex-col justify-between">
-            <Button className="border-2 border-solid border-black m-2 p-2">
-              View Community
-            </Button>
+        return (
+          <div
+            key={subscription.subredditId}
+            className="flex justify-between border border-solid border-gray-200 rounded-lg m-3 bg-gray-200"
+          >
+            {/* Left Side */}
+            <div className="flex flex-col m-3">
+              <span className="text-sm text-zinc-600">
+                {`Created By: u/${subscription.subredditCreator}`}
+              </span>
+              <span className="text-4xl font-ibm-plex-mono">{`r/${subscription.subredditName}`}</span>
+              <div className="my-5" />
+              <div className="flex">
+                <dt className="text-zinc-700">Creation Date:</dt>
+                <dd className="text-zinc-700 ml-1">
+                  <time dateTime={creationDate.toDateString()}>
+                    {format(creationDate, "MMMM d, yyyy")}
+                  </time>
+                </dd>
+              </div>
+            </div>
 
-            <span className="text-end m-2">Members: 2</span>
+            {/* Right Side */}
+            <div className="flex flex-col justify-between m-3">
+              <Button className="border-2 border-solid border-black p-2">
+                View Community
+              </Button>
+
+              <span className="text-end">{`Members: ${subscription.subredditMemberCount}`}</span>
+            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </>
   );
 };
