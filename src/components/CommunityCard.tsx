dc28d5a02960b56220e816f7da5de7d0f68bb632
Sora <sorazora1.4@gmail.com>
2023-08-12 23:02:21 -0700

    completed basic outline of community card

--- src/components/CommunityCard.tsx
+++ src/components/CommunityCard.tsx
@@ -1,5 +1,6 @@
 import { Subscription } from "@prisma/client";
 import { FC } from "react";
+import { Button } from "./ui/Button";
 
 interface CommunityCardProps {
   subscriptions: Subscription[];
@@ -11,14 +12,22 @@ const CommunityCard: FC<CommunityCardProps> = ({ subscriptions }) => {
       {subscriptions.map((subscription) => (
         <div className="flex justify-between border border-solid border-slate-500 rounded-lg m-3">
           {/* Left Side */}
-          <div className="flex flex-col">
-            <div className=""></div>
-            <div className="">Creation Date</div>
+          <div className="flex flex-col m-2">
+            <span className="text-sm text-zinc-500">
+              Created By: u/Soranosuke
+            </span>
+            <span className="text-4xl font-ibm-plex-mono">r/testing</span>
+            <div className="my-5" />
+            <div className="">Creation Date: August 23, 2023</div>
           </div>
 
           {/* Right Side */}
-          <div className="">
-            <button>View Community</button>
+          <div className="flex flex-col justify-between">
+            <Button className="border-2 border-solid border-black m-2 p-2">
+              View Community
+            </Button>
+
+            <span className="text-end m-2">Members: 2</span>
           </div>
         </div>
       ))}
