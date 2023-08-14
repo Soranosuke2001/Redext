9a662070b67239e742c4dc61d02b96491599ebd6
Sora <sorazora1.4@gmail.com>
2023-08-13 19:58:14 -0700

    changed from useQuery to useQueries

--- src/components/profile/UserInfo.tsx
+++ src/components/profile/UserInfo.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import { useQueries } from "@tanstack/react-query";
 import axios from "axios";
 import { useState } from "react";
 import { Skeleton } from "../ui/Skeleton";
@@ -17,22 +17,48 @@ interface UserInfoProps {
 const UserInfo = ({ userId }: UserInfoProps) => {
   const [navOption, setNavOption] = useState<string>("Joined Communities");
 
-  const { data, isFetched, isFetching, isError, error } = useQuery({
-    // Fetching user information from database
-    queryFn: async () => {
-      const { data } = (await axios.get(
-        `/api/user/communities?userId=${userId}`
-      )) as subbedSubreddit;
+  const [joinedCommunitiesQuery, upvotesQuery, downvotesQuery] = useQueries({
+    queries: [
+      {
+        queryKey: ["joinedCommunities"],
+        queryFn: async () => {
+          const { data } = (await axios.get(
+            `/api/user/communities?userId=${userId}`
+          )) as subbedSubreddit;
 
-      return data;
-    },
-    queryKey: ["search-user"],
+          return data;
+        },
+        enabled: navOption === "Joined Communities",
+      },
+      {
+        queryKey: ["upvotes"],
+        queryFn: async () => {
+          const { data } = (await axios.get(
+            `/api/user/votes?userId=${userId}&voteType=UP`
+          ));
+
+          return data;
+        },
+        enabled: navOption === "Upvotes",
+      },
+      {
+        queryKey: ["downvotes"],
+        queryFn: async () => {
+          const { data } = (await axios.get(
+            `/api/user/votes?userId=${userId}&voteType=DOWN`
+          ));
+
+          return data;
+        },
+        enabled: navOption === "Downvotes",
+      },
+    ],
   });
 
   return (
     <>
       {/* Loading Skeleton */}
-      {isFetching || !data ? (
+      {joinedCommunitiesQuery.isFetching || !joinedCommunitiesQuery.data ? (
         <div className="flex">
           <Skeleton className="w-[200px] h-[200px] rounded-full bg-slate-400" />
           <div className="ml-9">
@@ -45,9 +71,9 @@ const UserInfo = ({ userId }: UserInfoProps) => {
         <div>
           {/* Basic User Info */}
           <UserProfile
-            image={data.image}
-            createdAt={data.createdAt}
-            username={data.username}
+            image={joinedCommunitiesQuery.data.image}
+            createdAt={joinedCommunitiesQuery.data.createdAt}
+            username={joinedCommunitiesQuery.data.username}
           />
         </div>
       )}
@@ -58,14 +84,14 @@ const UserInfo = ({ userId }: UserInfoProps) => {
       </div>
 
       <MiniNavbar
-        isFetching={isFetching}
+        isFetching={joinedCommunitiesQuery.isFetching}
         setNavOption={setNavOption}
         navOption={navOption}
       />
 
       {/* Contents */}
       <div className="my-8">
-        {isFetching || !data ? (
+        {joinedCommunitiesQuery.isFetching || !joinedCommunitiesQuery.data ? (
           <div className="flex flex-col items-center">
             <Skeleton className="h-[150px] w-[90%] bg-slate-400 m-4 justify-center" />
             <Skeleton className="h-[150px] w-[90%] bg-slate-400 m-4 justify-center" />
@@ -73,7 +99,7 @@ const UserInfo = ({ userId }: UserInfoProps) => {
             <Skeleton className="h-[150px] w-[90%] bg-slate-400 m-4 justify-center" />
           </div>
         ) : (
-          <CommunityCard subscriptions={data.Subscription} />
+          <CommunityCard subscriptions={joinedCommunitiesQuery.data.Subscription} />
         )}
       </div>
     </>
