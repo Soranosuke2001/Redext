b525df51e07055041dc8b53addaf411f213967ed
sora <sorazora1.4@gmail.com>
2023-08-19 20:55:34 -0700

    added error messages for incomplete features

--- src/app/r/explore/page.tsx
+++ src/app/r/explore/page.tsx
@@ -1,11 +1,17 @@
-import { FC } from 'react'
+import { FC } from "react";
 
-interface pageProps {
-  
-}
+interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
-  return <div>Explore Communities Here!</div>
-}
+  return (
+    <div className="flex flex-col h-screen w-full items-center">
+      <h1 className="text-5xl pt-32">Feature not complete</h1>
+      <br />
+      <p className="">
+        Apologies. We are still developing this feature. Please try again later.
+      </p>
+    </div>
+  );
+};
 
-export default page
+export default page;
--- src/components/dark-mode/ThemeToggle.tsx
+++ src/components/dark-mode/ThemeToggle.tsx
@@ -4,6 +4,7 @@ import { useTheme } from "next-themes";
 import { FC, useEffect, useState } from "react";
 import { Button } from "../ui/Button";
 import { Moon, Sun } from "lucide-react";
+import { toast } from "@/hooks/use-toast";
 
 interface ThemeToggleProps {}
 
@@ -19,17 +20,30 @@ const ThemeToggle: FC<ThemeToggleProps> = ({}) => {
 
   return (
     <>
-      {theme === "light" ? (
-        // Need to add stuff here
-        // Implementing the dark theme
-        <Button variant="outline" onClick={() => setTheme("dark")}>
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-        </Button>
-      ) : (
-        <Button variant="outline" onClick={() => setTheme("light")}>
-          <Moon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-        </Button>
-      )}
+      <Button
+        variant="outline"
+        onClick={() =>
+          toast({
+            title: "Feature Unavailable",
+            description:
+              "Apologies. This feature is still in development. Please try again later.",
+            variant: "destructive",
+          })
+        }
+      >
+        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      </Button>
+      {/* {theme === "light" ? (
+          // Need to add stuff here
+          // Implementing the dark theme
+          <Button variant="outline" onClick={() => setTheme("dark")}>
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          </Button>
+        ) : (
+          <Button variant="outline" onClick={() => setTheme("light")}>
+            <Moon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          </Button>
+        )} */}
     </>
   );
 };
